a39f3e335ed4507d8f00a4fbb4ede3e72e9f9c91
giovanni <giovannisouza216@gmail.com>
2022-08-24 09:39:30 -0300

    bad

--- resources/views/restrict/mensagem/edit.blade.php
+++ resources/views/restrict/mensagem/edit.blade.php
@@ -36,7 +36,8 @@
     <div>
      <label for="img">Imagem</label>
      <input type="file" name="imagem" id="img" accept="image/*" required />
-     <img scr="{{Storage::url($mensagem->imagem)}}" alt="{{$mensagem->titulo}}" class="showImg" />
+     <!--img scr="{{Storage::url($mensagem->imagem)}}" alt="{{$mensagem->titulo}}" class="showImg" /> -->
+     <img src="{{$mensagem->imagem}}" alt="{{$mensagem->titulo}}" class="showImg" />
     </div>
     <button type="submit" class="button">Salvar</button>
 </form>
