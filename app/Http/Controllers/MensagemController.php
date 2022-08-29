813ecc78d67d917c690c8f9c8f1319148369dfc4
giovanni <giovannisouza216@gmail.com>
2022-08-29 16:12:44 -0300

    fe

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use App\Models\Mensagem;
 use App\Models\Topico;
 use Illuminate\Http\Request;
