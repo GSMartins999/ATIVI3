7bb54ab18c34572bc9082da48db97b7338276482
Giovanni <giovannisouza216@gmail.com>
2022-09-21 10:37:56 -0300

    LuanFlopou

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\Api\MensagemController;
 use App\Http\Controllers\Api\TopicoController;
+use App\Http\Controllers\Api\UseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,12 +19,14 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post("mensagem", [MensagemController::class, 'index']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // return $request->user();
 // });
 Route::middleware('auth:sanctum')->group(function () {
+    Route::put('/user', [UseController::class, 'update']);
     Route::get('/logout', [AuthController::class, 'logout']);
     Route::resource ("topico",TopicoController::class);
-    Route::resource ("mensagem", MensagemController::class);
+    Route::resource ("mensagem", MensagemController::class)->except("index");
    });
