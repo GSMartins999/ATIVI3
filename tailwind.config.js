7fce6c697016406a2939d29b948219aa8c11db1b
Giovanni <giovannisouza216@gmail.com>
2022-09-28 10:00:31 -0300

    funcionaaivai

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
@@ -14,6 +13,21 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+        },
+        colors:{
+            black: '#000000',
+            white: '#ffffff',
+            primary: '#36393f',
+            primaryLight: '#CFD8DC',
+            purple: '#1d1f22',
+            orange: '#FFB74D',
+            orangeLight: '#FFE9CA',
+            fundo: "#383646",
+            perfil:"#B5B2BF",
+            pop: "#3C3945",
+            postbotao: "#3C3945",
+            Card: "#777382",
+            qrCode: "#383646"
         },
     },
 
